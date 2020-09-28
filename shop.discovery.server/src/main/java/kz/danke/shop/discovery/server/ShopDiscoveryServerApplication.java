d6f241b5dba2b0946f2da6521f7c9352c7ae4bb5
Vladislav Dankevich <vielendanke1991@gmail.com>
2020-09-28 22:26:50 +0600

    config server added

--- shop.discovery.server/src/main/java/kz/danke/shop/discovery/server/ShopDiscoveryServerApplication.java
+++ shop.discovery.server/src/main/java/kz/danke/shop/discovery/server/ShopDiscoveryServerApplication.java
@@ -1,15 +1,25 @@
 package kz.danke.shop.discovery.server;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class ShopDiscoveryServerApplication {
 
+	@Value("${test.url}")
+	private String testUrl;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ShopDiscoveryServerApplication.class, args);
 	}
 
+	@EventListener(ContextRefreshedEvent.class)
+	public void refreshedEvent() {
+		System.out.println(testUrl);
+	}
 }
