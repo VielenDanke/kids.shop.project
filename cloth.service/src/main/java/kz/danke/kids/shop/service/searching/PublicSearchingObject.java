d4b08d95343cfb19f5f9fa83e086a996dfd7bf3d
Vladislav Dankevich <vielendanke1991@gmail.com>
2020-11-04 22:21:41 +0600

    extra fields for searching

--- cloth.service/src/main/java/kz/danke/kids/shop/service/searching/PublicSearchingObject.java
+++ cloth.service/src/main/java/kz/danke/kids/shop/service/searching/PublicSearchingObject.java
@@ -14,4 +14,8 @@ public class PublicSearchingObject {
     private String name;
     private String description;
     private String material;
+    private String sex;
+    private Integer age;
+    private String height;
+    private String color;
 }
