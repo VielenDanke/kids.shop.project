3c8b3ea770bb15bed60bc8cd26f9f250f9c6b992
Vladislav Dankevich <vielendanke1991@gmail.com>
2020-12-31 14:16:57 +0600

    test user repository setup

--- user.service/src/test/java/kz/danke/user/service/repository/AbstractRepositoryLayer.java
+++ user.service/src/test/java/kz/danke/user/service/repository/AbstractRepositoryLayer.java
@@ -1,15 +1,25 @@
 package kz.danke.user.service.repository;
 
+import kz.danke.user.service.document.Authorities;
+import kz.danke.user.service.document.User;
 import org.junit.ClassRule;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.data.elasticsearch.client.reactive.ReactiveElasticsearchClient;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Collections;
+import java.util.Optional;
+import java.util.UUID;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -18,6 +28,25 @@ import org.testcontainers.elasticsearch.ElasticsearchContainer;
 public abstract class AbstractRepositoryLayer {
 
     private static final String ELASTICSEARCH_DOCKER = "docker.elastic.co/elasticsearch/elasticsearch:7.6.2";
+
+    @Autowired
+    protected ReactiveUserRepository userRepository;
+    @Autowired
+    protected ReactiveElasticsearchClient reactiveElasticsearchClient;
+
+    protected String testData = "test";
+
+    protected User testUser = User.builder()
+                .id(UUID.randomUUID().toString())
+                .address(testData)
+                .authorities(Collections.singleton(Authorities.ROLE_USER.name()))
+                .city(testData)
+                .firstName(testData)
+                .lastName(testData)
+                .password(testData)
+                .phoneNumber(testData)
+                .username(testData)
+                .build();
 
     @ClassRule
     public static ElasticsearchContainer container = new ElasticsearchContainer(ELASTICSEARCH_DOCKER);
--- user.service/src/test/java/kz/danke/user/service/repository/UserRepositoryTest.java
+++ user.service/src/test/java/kz/danke/user/service/repository/UserRepositoryTest.java
@@ -1,17 +1,53 @@
 package kz.danke.user.service.repository;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.Optional;
 
 public class UserRepositoryTest extends AbstractRepositoryLayer {
 
     @BeforeEach
     public void setup() {
-
+        Mono.just("cloth.shop.user")
+                .filter(index -> {
+                    Optional<Boolean> existOptional = reactiveElasticsearchClient.indices().existsIndex(getIndexRequest ->
+                            getIndexRequest.indices(index)).blockOptional();
+                    return existOptional.map(aBoolean -> !aBoolean).orElse(true);
+                })
+                .flatMap(index -> reactiveElasticsearchClient.indices().createIndex(createIndexRequest ->
+                        createIndexRequest.index(index)))
+                .block();
+        userRepository.deleteAll().block();
+        userRepository.save(testUser).block();
     }
 
     @Test
-    public void test() {
+    public void userRepository_SaveNewUser() {
+        userRepository.deleteAll().block();
 
+        StepVerifier.create(userRepository.save(testUser))
+                .expectSubscription()
+                .expectNextMatches(u -> u.getId().equals(testUser.getId()))
+                .verifyComplete();
+    }
+
+    @Test
+    public void userRepository_FindUserByUsername() {
+        StepVerifier.create(userRepository.findByUsername(testData))
+                .expectSubscription()
+                .expectNextMatches(u -> u.getUsername().equals(testData))
+                .verifyComplete();
+    }
+
+    @Test
+    public void userRepository_FindUserByUsername_NotFound() {
+        StepVerifier.create(userRepository.findByUsername("invalid"))
+                .expectSubscription()
+                .expectNextCount(0)
+                .verifyComplete();
     }
 }
