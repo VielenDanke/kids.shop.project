5ea22e8e2d184a7e6a3e83a0a8cfa37b3763b09d
Vladislav Dankevich <vielendanke1991@gmail.com>
2021-02-16 12:19:36 +0600

    kafka and zookeeper for kubernetes deployment added

--- edge-service/src/main/java/kz/danke/edge/service/configuration/GatewayConfig.java
+++ edge-service/src/main/java/kz/danke/edge/service/configuration/GatewayConfig.java
@@ -45,7 +45,7 @@ public class GatewayConfig {
         final CorsConfiguration corsConfig = new CorsConfiguration();
         corsConfig.setAllowedOrigins(Collections.singletonList("*"));
         corsConfig.setMaxAge(3600L);
-        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST"));
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         corsConfig.addAllowedHeader("*");
 
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
